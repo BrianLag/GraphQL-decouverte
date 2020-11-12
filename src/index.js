3702ec1191266599e833b8e71b8ecb1ac0024fd5
Brian Lagaude <brian.lagaude@gmail.com>
2020-11-12 10:52:28 +0100

    challenge terminé

--- src/index.js
+++ src/index.js
@@ -1,15 +1,72 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql',
+  cache: new InMemoryCache()
+});
+
+const SPACEX_LAUNCHES = gql`
+  query GetLaunches {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
+
+function SpacexLaunches() {
+  const { loading, error, data } = useQuery(SPACEX_LAUNCHES);
+
+  if (loading) return <p>Loading Launches</p>;
+  if (error) return <p>Error in fetching :(</p>;
+
+  return data.launches.map(({ launch_date_utc, launch_success, rocket, links, details }) => (
+    <div key={rocket.rocket_name}>
+      <h2>{rocket.rocket_name}</h2>
+      <ul>
+        <li>
+          {launch_date_utc}
+        </li>
+        <li>{launch_success  ?  "Successful Lanch" : "Failed Launch"}</li>
+        <li>
+          <a href={links.video_link}>
+          {links.video_link}
+          </a>
+        </li>
+        <li>
+          {details ? details : "No details available"}
+        </li>
+      </ul>
+    </div>
+  ));
+}
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Wilder Apollo app 🚀</h2>
+      </div>
+      <SpacexLaunches />
+    </ApolloProvider>
+  );
+}
+
+render(<App />, document.getElementById('root'));
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
